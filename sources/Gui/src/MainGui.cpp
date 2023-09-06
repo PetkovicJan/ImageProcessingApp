2db157a70ed5ce874d00ed4e6152a61086910971
Jan <jpetko1@gmail.com>
2023-09-06 19:06:39 +0200

    Created basic application with two buttons in the toolbar and an image display.

--- sources/Gui/src/MainGui.cpp
+++ sources/Gui/src/MainGui.cpp
@@ -1,9 +1,55 @@
+#include <Gui/MainGui.hpp>
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QToolBar>
+#include <QVBoxLayout>
+#include <QGraphicsView>
+
 #include <iostream>
 
+MainWidget::MainWidget()
+{
+  this->setFixedSize(800, 600);
+
+  // Create and configure widgets.
+  auto toolbar = new QToolBar();
+  toolbar->setMovable(false);
+  toolbar->setAllowedAreas(Qt::TopToolBarArea);
+
+  auto load_action = toolbar->addAction("Load");
+  auto save_action = toolbar->addAction("Save");
+
+  auto display = new QGraphicsView();
+  display->setScene(new QGraphicsScene(this));
+
+  // Configure layout.
+  auto main_layout = new QVBoxLayout();
+  this->setLayout(main_layout);
+
+  main_layout->addWidget(toolbar);
+  main_layout->addWidget(display);
+
+  // Make connections.
+  QObject::connect(load_action, &QAction::triggered, [] 
+    {
+      std::cout << "Load clicked\n";
+    });
+
+  QObject::connect(save_action, &QAction::triggered, [] 
+    {
+      std::cout << "Save clicked\n";
+    });
+}
 
 int main(int argc, char** argv)
 {
-  std::cout << "Hello World!\n";
+  QApplication a(argc, argv);
 
-  return 0;
+  QMainWindow main_window;
+  main_window.setCentralWidget(new MainWidget());
+  main_window.move(200, 200);
+  main_window.show();
+
+  return a.exec();
 }
