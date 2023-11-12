9b0d97221c4304ff9c7b549b63b0f6f4f8aa9df9
Jan <jpetko1@gmail.com>
2023-11-12 13:24:55 +0100

    Added functions to transform from Image2d to QImage and vice-versa.

--- sources/Gui/src/MainControl.cpp
+++ sources/Gui/src/MainControl.cpp
@@ -2,11 +2,48 @@
 
 #include <QFileDialog>
 
+#include <iostream>
+
 namespace detail
 {
-  void create_array2d_from_image(QImage const& img, Image2d<float>& arr)
+  void create_image2d_from_qimage(QImage const& qimg, Image2d<float>& img)
   {
+    img.alloc(qimg.height(), qimg.width());
 
+    foreach_y(img, y)
+    {
+      // QImage has a method pixel() to access each individual pixel value, 
+      // but the documentation suggests the scanLine() method in case where 
+      // we want to access all the pixels for performance reasons.
+      const auto row = reinterpret_cast<QRgb const*>(qimg.constScanLine(y));
+      foreach_x(img, x)
+      {
+        const auto& rgb = row[x];
+        img(y, x) = qGray(rgb);
+      }
+    }
+  }
+
+  QImage create_qimage_from_image2d(Image2d<float> const& img)
+  {
+    QImage qimg(img.width(), img.height(), QImage::Format_RGB32);
+
+    foreach_y(img, y)
+    {
+      // QImage has a method pixel() to access each individual pixel value, 
+      // but the documentation suggests the scanLine() method in case where 
+      // we want to access all the pixels for performance reasons.
+      const auto row = reinterpret_cast<QRgb*>(qimg.scanLine(y));
+      foreach_x(img, x)
+      {
+        const auto val = img(y, x);
+        QColor color;
+        color.setRgbF(val, val, val);
+        row[x] = color.rgb();
+      }
+    }
+
+    return qimg;
   }
 }
 
@@ -19,11 +56,11 @@ MainControl::MainControl(MainWidget* main_widget)
 
       if (image_name.isNull()) return;
 
-      QImage img;
-      img.load(image_name);
+      QImage loaded_img;
+      loaded_img.load(image_name);
 
-      detail::create_array2d_from_image(img, current_img_);
+      detail::create_image2d_from_qimage(loaded_img, current_img_);
 
-      main_widget->setImage(QPixmap::fromImage(img));
+      main_widget->setImage(QPixmap::fromImage(loaded_img));
     });
 }
